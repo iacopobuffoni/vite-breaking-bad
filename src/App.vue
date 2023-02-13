5a4f6ddea0277359b10410ebd7c78d447571fce7
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-02-13 09:33:29 +0100

    Updating

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 	import AppMain from './components/AppMain.vue';
 	import AppFooter from './components/AppFooter.vue';
 	import axios from 'axios';
+	import { store } from './store.js';
 	export default {
 		name: 'App',
   		components : {
@@ -15,7 +16,8 @@
 		data() {
 
 			return {
-				cards: []
+				cards: [],
+				store
 			}
 
 		},
@@ -23,7 +25,7 @@
 		created() {
 			axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').then((response) => {
 				console.log(response.data.data.slice(0,20));
-				this.cards = response.data.data.slice(0,20);
+				this.store.cards = response.data.data.slice(0,20);
 			});
 		}
 	
@@ -32,7 +34,7 @@
 
 <template>
   	<AppHeader/>
-	<AppMain :cardsList="cards" :cardsCount="cards.length"/>
+	<AppMain :cardsList="store.cards" :cardsCount="store.cards.length"/>
 	<AppFooter/>
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,12 @@
 <script>
+import { store } from '../store.js';
 export default {
     name: 'AppMain',
-    props: {
-        cardsList: {
-            type: Array,
-            deafult: []
-        },
-        cardsCount: {
-            type: Number,
-            default: 0
+    data() {
+        return {
+            store
         }
-    }
+    },
 }
 </script>
 
@@ -18,15 +14,21 @@ export default {
     <main>
         <form action="">
             <select name="type" id="" class="ms-5 mt-3">
+                <option value="dd"> D/D </option>
+                <option value="gem"> Gem- </option>
+                <option value="cipher"> Cipher </option>
+                <option value="darkmagician"> Dark Magician </option>
+                <option value="alien"> Alien </option>
+                <option value="alien"> Alien </option>
                 <option value="alien"> Alien </option>
             </select>
         </form>
         <div class="container p-5">
             <div class="row">
-                <p> Found {{ cardsCount }} cards </p>
+                <p> Found {{ store.cards.length }} cards </p>
             </div>
             <div class="row row-cols-lg-5">
-                <div class="col text-center mb-5" v-for="cards in cardsList">
+                <div class="col text-center mb-5" v-for="cards in store.cards">
                     <div>
                         <img :src="`${cards.card_images[0].image_url}`" alt="" class="img-fluid">
                     </div>
